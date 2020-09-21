45efb7d86f7991492be49f1a856fc0d270200f6e
DanzT <danztty@gmail.com>
2020-09-22 02:10:00 +0800

    [MOD] initial crud :fre:

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-const express = require('express')
-const morgan = require('morgan')
+const app = require('./app')
 
-const app = express();
-
-app.use(morgan('dev'))
-
-app.listen(3000);
-console.log('Server on port', 3000);
+app.listen(app.get('port'));
+console.log('Server on port', app.get('port'));
